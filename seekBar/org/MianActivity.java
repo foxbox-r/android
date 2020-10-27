f0181d6024150e4a960df6d4b1e2b35bb7bb6799
rsua <foxboxr@gmail.com>
2020-10-27 17:21:38 +0900

    seekBar 에 대한 커밋

---
+++ seekBar/org/MianActivity.java
@@ -0,0 +1,33 @@
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView text;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        text = findViewById(R.id.textView);
+        seekBar = findViewById(R.id.seekBar);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                text.setText("지정된 값 : "+i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
