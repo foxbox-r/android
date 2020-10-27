02e3bdc70e6bef17784a5e3ae4f617977045d0dc
rsua <foxboxr@gmail.com>
2020-10-27 17:07:37 +0900

    webView 에 대한 커밋

---
+++ webView/org/MainActivity.java
@@ -0,0 +1,48 @@
+package org.techtown.web;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edit;
+    WebView webView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edit = findViewById(R.id.edit);
+        webView = findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient(){
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        });
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                webView.loadUrl(edit.getText().toString());
+            }
+        });
+    }
+
+
+}
