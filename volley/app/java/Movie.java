ce86e55a2bb8d3495e0c22d9476f6f2c46eb8e70
rsua <foxboxr@gmail.com>
2020-11-17 19:38:45 +0900

    volley - get json api && show recyclerView ;

---
+++ volley/app/java/Movie.java
@@ -0,0 +1,19 @@
+package org.techtown.volley;
+
+public class Movie {
+    String rnum;
+    String rank;
+    String rankInten;
+    String rankOldAndNew;
+    String movieCd;
+    String movieNm;
+    String movieDt;
+    String salsesAmt;
+    String salsesShare;
+    String salsesInten;
+    String salsesChange;
+    String salsesAcc;
+    String audiCnt;
+    String scmCnt;
+    String showCnt;
+}
---
+++ volley/app/java/MovieListResult.java
@@ -0,0 +1,9 @@
+package org.techtown.volley;
+import java.util.ArrayList;
+
+public class MovieListResult {
+    String boxofficeType;
+    String gshowRange;
+    ArrayList<Movie> dailyBoxOfficeList = new ArrayList<Movie>();
+
+}
